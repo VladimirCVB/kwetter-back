7b6580dadf911db07fab9e9c38e0f9fbaddd31a9
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2022-03-23 12:42:52 +0200

    swagger setup for all services

--- post-service/src/main.ts
+++ post-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Post service API')
+    .setDescription('The post service API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
+
 bootstrap();
