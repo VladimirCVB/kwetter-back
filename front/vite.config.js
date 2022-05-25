ba10b6fbc59a82a055ff6257f14b725d1611ab2a
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2022-05-25 22:43:02 +0300

    Running Front-End Docker

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,6 +6,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 8080
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
